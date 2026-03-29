8bd345598b4aade353778661db084aca639214c6
Febrianti10 <fbrntkhmrh@gmail.com>
2026-03-29 14:14:20 +0700

    downgrade prisma ke 5.22 sesuai instruksi

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,7 +1,7 @@
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from "../src/generated/prisma/client";
 
-const adapter = new PrismaLibSQL({
+const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL!,
   authToken: process.env.DB_AUTH_TOKEN,
 });
--- apps/backend/tsdown.config.ts
+++ apps/backend/tsdown.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   deps: {
-    alwaysBundle: [
-      "shared",
-      "@prisma/adapter-libsql",
-      "@libsql/client",
-      "@libsql/core",
-    ],
-    neverBundle: ["@libsql/linux-x64-gnu"],
+    alwaysBundle: ["shared"],
   },
 });
