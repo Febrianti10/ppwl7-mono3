5bbbd3d2ad32960562a1477174a0e35a3ecfdcf7
Febrianti10 <fbrntkhmrh@gmail.com>
2026-03-29 13:45:42 +0700

    fix bundle adapter-libsql

--- apps/backend/tsdown.config.ts
+++ apps/backend/tsdown.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  // external diletakkan di luar deps
-  external: [
-    "@libsql/client",
-    "@libsql/core",
-    "@prisma/client",
-    "@prisma/adapter-libsql",
-    "@libsql/win32-x64-msvc",
-  ],
   deps: {
-    alwaysBundle: ["shared"],
+    alwaysBundle: [
+      "shared",
+      "@prisma/adapter-libsql",
+      "@libsql/client",
+      "@libsql/core",
+    ],
   },
 });
