5818edcc79c805e74062cc79ac8c9998ca1cf18d
Febrianti10 <fbrntkhmrh@gmail.com>
2026-03-29 14:34:08 +0700

    fix PrismaLibSQL usage v5

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,9 +1,12 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaLibSql({
+const client = createClient({
   url: process.env.DATABASE_URL!,
   authToken: process.env.DB_AUTH_TOKEN,
 });
+
+const adapter = new PrismaLibSQL(client);
 
 export const prisma = new PrismaClient({ adapter });
