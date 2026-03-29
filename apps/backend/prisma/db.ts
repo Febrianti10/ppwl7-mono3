9400dc4bd05cfb172dec0dfadae052c820377aa2
Febrianti10 <fbrntkhmrh@gmail.com>
2026-03-29 13:53:07 +0700

    fix prisma version dan db.ts

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,11 +1,9 @@
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from "../src/generated/prisma/client";
 
-// Konfigurasi adapter agar Prisma bisa "ngobrol" dengan Turso
 const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL!,
   authToken: process.env.DB_AUTH_TOKEN,
 });
 
-// Export prisma agar bisa dipakai di file index.ts atau seeder
 export const prisma = new PrismaClient({ adapter });
